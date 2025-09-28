779d85ca26b464ffcca145a15d492f76b3d04682
C. Hecktor <checktor@posteo.de>
2025-09-28 22:09:20 +0200

    Feature: Responsiveness (#2)
    * Integrate Bootstrap to implement responsive design.
    * Minor adjustments to "Raspberry Pi NAS server" blog post.

---
+++ projects/app/src/app/common/blog-post-card.interface.ts
@@ -0,0 +1,7 @@
+export interface BlogPostCard {
+  link: string,
+  title: string,
+  content: string,
+  imgSrc: string,
+  imgAlt: string
+}
--- projects/app/src/app/overview/overview.component.ts
+++ projects/app/src/app/overview/overview.component.ts
@@ -1,19 +1,37 @@
 import {Component} from '@angular/core';
-import {MatCard, MatCardContent, MatCardHeader, MatCardImage, MatCardTitle} from '@angular/material/card';
 import {RouterLink} from '@angular/router';
+import {BlogPostCard} from '../common/blog-post-card.interface';
+import {MatCard, MatCardContent, MatCardHeader, MatCardImage, MatCardTitle} from '@angular/material/card';
 
 @Component({
   selector: 'app-overview',
   imports: [
+    RouterLink,
     MatCard,
-    MatCardContent,
     MatCardHeader,
     MatCardTitle,
-    RouterLink,
-    MatCardImage
+    MatCardImage,
+    MatCardContent
   ],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.scss'
 })
 export class OverviewComponent {
+
+  protected blogPostCards: BlogPostCard[] = [
+    {
+      link: 'raspberry-pi-nas-server',
+      title: 'Raspberry Pi NAS server',
+      content: 'Searching for security and performance best practices when configuring Samba server on Raspberry Pi.',
+      imgSrc: '/imgs/raspberry-pi.jpg',
+      imgAlt: 'Raspberry Pi'
+    },
+    {
+      link: 'under-construction',
+      title: 'Under construction',
+      content: 'Stay tuned!',
+      imgSrc: '/imgs/under-construction.jpg',
+      imgAlt: 'Under construction'
+    }
+  ]
 }
